1d490c71847800364bfa129d7d164d334c3d80c8
Chris Allick <chrisallick@gmail.com>
2013-04-30 19:14:50 -0700

    initial commit. hopefully last.

---
+++ README.md
@@ -0,0 +1,4 @@
+The Longest Nipple Ring
+======
+
+nice.
---
+++ js/main.js
@@ -0,0 +1,50 @@
+centerx = function() {
+	$(".center-x").each(function(){
+		$(this).css({
+			left: $(document).width()/2-$(this).width()/2,
+			opacity: 1
+		});
+	});
+}
+
+centery = function() {
+	$(".center-y").each(function(){
+		$(this).css({
+			top: $(document).height()/2-$(this).height()/2,
+			opacity: 1
+		});
+	});
+}
+
+$(document).ready(function() {
+    var t = setTimeout(function(){
+        $(this).scrollTop(0);
+    }, 250);
+
+	centerx();
+	centery();
+
+	$("#chain").css({
+		left: $("#nipples").position().left
+	});
+
+	$("#dogtag").css({
+		left: $("#nipples").position().left - 25
+	}).click(function(){
+        $("body").animate({
+        	scrollTop: 0
+        }, 300);
+	});
+
+	$(window).resize(function(){
+		centerx();
+
+		$("#chain").css({
+			left: $("#nipples").position().left
+		});
+
+		$("#dogtag").css({
+			left: $("#nipples").position().left - 25
+		});
+	})
+});
